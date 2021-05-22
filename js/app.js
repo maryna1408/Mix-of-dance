a4fd6953d79f88090eb121c8b95de9be988dbe91
maryna1408 <cabanmarina9@gmail.com>
2021-05-23 01:22:52 +0300

    Classes
    - add Classes to index.html
    - create the folder js and file app.js
    - plug swiper-wripper
    - create ClassesSlider in app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const classesSlider = new Swiper('.classes-slider', {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
