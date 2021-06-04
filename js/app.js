39c2f91e40219045c13af60431702074451d2d24
maryna1408 <cabanmarina9@gmail.com>
2021-06-04 18:02:55 +0300

    Gallery

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const classesSlider = new Swiper('.classes-slider', {
   slidesPerView: 1,
   loop: true,
   autoplay: {
-      delay: 5000,
+      delay: 3000,
   },
   navigation: {
       nextEl: '.swiper-button-next',
@@ -10,3 +10,16 @@ const classesSlider = new Swiper('.classes-slider', {
   },
   speed: 700,
 })
+
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
